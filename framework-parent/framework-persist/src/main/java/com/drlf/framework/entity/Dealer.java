8785e888630ffb8a946e98daf18e2271bbaf286c
Edwin Long <lfedwin@gmail.com>
2012-04-11 14:32:44 +0800

    update test2

--- framework-parent/framework-persist/src/main/java/com/drlf/framework/entity/Dealer.java
+++ framework-parent/framework-persist/src/main/java/com/drlf/framework/entity/Dealer.java
@@ -4,7 +4,7 @@ public class Dealer {
 	String name;
 
 	public String getName() {
-		return name + ". edit by lf! 3nd";
+		return name + ". edit by lf! 4nd";
 	}
 
 	public void setName(String name) {
